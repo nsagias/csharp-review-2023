178f52f0c118b82091f606b54013ce44fc5a2484
Nick <nsagias@gmail.com>
2023-03-05 01:13:56 -0500

    loop through assembly methods

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+Assembly? myApp = Assembly.GetEntryAssembly();
 
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    Assembly a = Assembly.Load(name);
+
+    int methodCount = 0;
+
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        methodCount += t.GetMethods().Count();
+    }
+
+    Console.WriteLine(
+        "{0:N0} types with {1:N0} methods in {2} assembly.",
+        arg0: a.DefinedTypes.Count(),
+        arg1: methodCount,
+        arg2: name.Name
+    );
+}
