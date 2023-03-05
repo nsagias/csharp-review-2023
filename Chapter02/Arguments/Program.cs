75f2d81987dc2b342ee159ff2c3ee412e41c99ab
Nick <nsagias@gmail.com>
2023-03-05 18:51:03 -0500

    get arguments and loop through arguments

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿WriteLine($"There are {args.Length} arguments");
 
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
