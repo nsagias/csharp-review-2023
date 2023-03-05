d05cd5ef986f86edb752b863552f835536d27b67
Nick <nsagias@gmail.com>
2023-03-05 18:59:41 -0500

    add logic from input

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -1,6 +1,31 @@
-﻿WriteLine($"There are {args.Length} arguments");
+﻿// Print number of arugments passed
+WriteLine($"There are {args.Length} arguments");
 
+// Loop through arguments and print arguments passed
 foreach (string arg in args)
 {
     WriteLine(arg);
 }
+
+
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and curser size");
+    WriteLine("For example: dotnet run red yellow");
+    return;
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0],
+    ignoreCase: true
+);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1],
+    ignoreCase: true
+);
+
+CursorSize = int.Parse(args[2]);
