3ef12cddf8cf19e3113deed9c3174f27f13e8195
Nick <nsagias@gmail.com>
2023-03-05 01:31:15 -0500

    add char and strings

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -1,3 +1,19 @@
 ﻿double heightInMeters = 1.88;
 Console.WriteLine($"The variable {nameof(heightInMeters)} has the number value {heightInMeters}");
 
+// chars
+// use single quotes
+char letter = 'A';
+char digit = '1';
+char symbol = '$';
+//char userChoice =  GetSomeKeyStroke()// fake functions
+
+// String
+string firstName = "Nick";
+string lastName = "Sagias";
+string phoneNumber = "(123) 555-5555";
+string horizontalLine = new('-', count: 74);
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+string grinningEmoji = char.ConvertFromUtf32(0x1F600);
+Console.WriteLine(grinningEmoji);
