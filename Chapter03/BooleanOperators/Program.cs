85cdcea18725c7db51d619b9f9316dbcd2b9ca3d
Nick <nsagias@gmail.com>
2023-03-05 21:01:29 -0500

    logical boolean operators

--- Chapter03/BooleanOperators/Program.cs
+++ Chapter03/BooleanOperators/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿bool a = true;
+bool b = false;
+
+WriteLine($"AND | a | b ");
+WriteLine($"a   | {a & a} | {a & b}");
+WriteLine($"b   | {b & a} | {b & b}");
+WriteLine($"---");
+
+WriteLine($"OR");
+WriteLine($"a   | {a | a} | {a | b}");
+WriteLine($"b   | {b | a} | {b | b}");
+WriteLine($"---");
+
+WriteLine($"XOR");
+WriteLine($"a   | {a ^ a} | {a ^ b}");
+WriteLine($"b   | {b ^ a} | {b ^ b}");
+
+ReadKey();
