781665180c5281e11267f7c0a02d139d773d499c
Nick <nsagias@gmail.com>
2023-03-06 02:30:19 -0500

    add switch statement example

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,4 +1,5 @@
-﻿string password = "ninja";
+﻿// if else control flow
+string password = "ninja";
 
 if (password.Length < 8)
 {
@@ -8,3 +9,29 @@ else
 {
     WriteLine("Your password is strong");
 }
+
+// switch control flow
+int randomNumber = Random.Shared.Next(1, 7);
+WriteLine($"Random number generated {randomNumber}");
+switch (randomNumber)
+{
+    case 1:
+        WriteLine("One");
+        break;
+    case 2:
+        WriteLine("Two");
+        goto case 1;
+    case 3: // falls through
+    case 4:
+        WriteLine("Three or Four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+} // end of switch
+
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
