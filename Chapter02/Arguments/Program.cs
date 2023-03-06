e6b5291da98317216b60288a12bde6ba53004c79
Nick <nsagias@gmail.com>
2023-03-05 19:04:45 -0500

    add conditional rendering based on operation system

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -9,23 +9,44 @@ foreach (string arg in args)
 
 
 
-if (args.Length < 3)
+
+
+
+if (OperatingSystem.IsWindows())
 {
-    WriteLine("You must specify two colors and curser size");
-    WriteLine("For example: dotnet run red yellow");
-    return;
+    // execute code that only works on Windows
+    if (args.Length < 3)
+    {
+        WriteLine("You must specify two colors and curser size");
+        WriteLine("For example: dotnet run red yellow");
+        return;
+    }
+
+    ForegroundColor = (ConsoleColor)Enum.Parse(
+        enumType: typeof(ConsoleColor),
+        value: args[0],
+        ignoreCase: true
+    );
+
+    BackgroundColor = (ConsoleColor)Enum.Parse(
+        enumType: typeof(ConsoleColor),
+        value: args[1],
+        ignoreCase: true
+    );
+
+    CursorSize = int.Parse(args[2]);
 }
+else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+{
+    // execute code that only works on Windows 10 or later
+}
+else if (OperatingSystem.IsIOSVersionAtLeast(major: 14, minor: 5))
+{
+    // execute code that only works on iOS 14.5 or later
 
-ForegroundColor = (ConsoleColor)Enum.Parse(
-    enumType: typeof(ConsoleColor),
-    value: args[0],
-    ignoreCase: true
-);
-
-BackgroundColor = (ConsoleColor)Enum.Parse(
-    enumType: typeof(ConsoleColor),
-    value: args[1],
-    ignoreCase: true
-);
-
-CursorSize = int.Parse(args[2]);
+    WriteLine("Your code won't rung");
+}
+else if (OperatingSystem.IsBrowser())
+{
+    // execute code that only works in the browser with Blazor
+}
