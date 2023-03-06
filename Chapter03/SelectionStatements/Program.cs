85bedc37e8c01cf310b815f54433602eb4019559
Nick <nsagias@gmail.com>
2023-03-06 02:22:41 -0500

    if logic example

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string password = "ninja";
+
+if (password.Length < 8)
+{
+    WriteLine("Password is too short.  Should be atleast 8 charaters");
+}
+else
+{
+    WriteLine("Your password is strong");
+}
