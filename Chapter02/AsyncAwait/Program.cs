1636e293d2bbc622307aad8bc30002129eb39988
Nick <nsagias@gmail.com>
2023-03-05 19:14:50 -0500

    add async await code for http

--- Chapter02/AsyncAwait/Program.cs
+++ Chapter02/AsyncAwait/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿HttpClient client = new();
+HttpResponseMessage response =
+    await client.GetAsync("http://www.apple.com");
+Console.WriteLine("Apple's home page has {0:N0} bytes", response.Content.Headers.ContentLength);
 
+ReadKey();
