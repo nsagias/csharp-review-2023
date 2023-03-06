1e7fdf258518e3268e6287c1b4ac3c639f45e8fd
Nick <nsagias@gmail.com>
2023-03-05 21:25:51 -0500

    create binary string

--- Chapter03/BooleanOperators/Functions1.cs
+++ Chapter03/BooleanOperators/Functions1.cs
@@ -9,6 +9,10 @@ namespace BooleanOperators
 			return true;
 		}
 
+		public static string ToBinaryString(int value)
+		{
+			return Convert.ToString(value, toBase: 2).PadLeft(8, '0');
+		}
 	}
 }
 
