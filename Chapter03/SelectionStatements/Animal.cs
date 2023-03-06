42bd14f2e579b312fcefa8965704f6697a26f481
Nick <nsagias@gmail.com>
2023-03-06 02:34:54 -0500

    create class Animal, Cat, Spider

---
+++ Chapter03/SelectionStatements/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SelectionStatements
+{
+	public class Animal
+	{
+		public string? Name;
+		public DateTime TodaysDate;
+		public byte Legs;
+	}
+
+	public class Cat : Animal
+	{
+		public bool IsDometic;
+	}
+
+	public class Spider : Animal
+	{
+		public bool IsPoisonous;
+	}
+}
+
