bd3d080943564e444dff7d1889c92879195b40e6
Nick <nsagias@gmail.com>
2023-03-06 03:14:02 -0500

    add do while and for statements

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// do while
+string? password;
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+}
+while (password != "pwd");
+WriteLine("Correct!");
+
+// for statement
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
