28a70e0ecd2816aef00c0229d1ff0682449bfeee
Nick <nsagias@gmail.com>
2023-03-07 15:49:28 -0500

    create try catch example to catch exceptions

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿WriteLine("Before parsing");
+Write("Enter a number: ");
+string? input = ReadLine();
+try
+{
+    int? numInput = int.Parse(s: input);
+    WriteLine($"The number selected is {numInput}");
+} catch (Exception error)
+{
+    WriteLine($"Exception Type: {error.GetType() }");
+    WriteLine($"Exception Message: {error.Message}");
+}
+WriteLine("After parsing");
