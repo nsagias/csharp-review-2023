32e543fdecc3a489cbaf210e0dea9131cfe49d95
Nick <nsagias@gmail.com>
2023-03-06 23:44:26 -0500

    converting binary object as bytes and converting to base64

--- Chapter03/Casting/Program.cs
+++ Chapter03/Casting/Program.cs
@@ -12,3 +12,32 @@ foreach (double num in doublesArray)
 {
     WriteLine($"Looping through array convert {num } to integer example: {ToInt32(num)}");
 }
+
+WriteLine("---");
+WriteLine("Converting any type to string");
+int num2 = 12;
+bool booleanValue = true;
+DateTime timeNow = DateTime.Now;
+object myObj = new();
+WriteLine(num2);
+WriteLine(booleanValue);
+WriteLine(timeNow);
+WriteLine(myObj);
+
+
+WriteLine("---");
+WriteLine("Converting binary type to string");
+byte[] binaryObject = new byte[128];
+
+Random.Shared.NextBytes(binaryObject);
+WriteLine(binaryObject);
+WriteLine("loop through binary object as bites");
+
+for (int index = 0; index < binaryObject.Length; index++)
+{
+    Write($"{binaryObject[index]:X} ");
+}
+
+WriteLine();
+string encoded = ToBase64String(binaryObject);
+WriteLine($"Binary Object as Base64: {encoded}");
