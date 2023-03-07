5a73f2b43a719e96189b009d11251a6f93020134
Nick <nsagias@gmail.com>
2023-03-06 22:34:37 -0500

    loop through multi-dimentional array

--- Chapter03/Arrays/Program.cs
+++ Chapter03/Arrays/Program.cs
@@ -14,7 +14,30 @@ names[3] = "Nix3";
 
 string?[] names2 = new[] { names[0], names[1], names[2], names[3] };
 
-for (int i = 0; i < names2.Length; i++)
+//for (int i = 0; i < names2.Length; i++)
+//{
+//    WriteLine(names2[i]);
+//}
+
+
+// Multi-Dimensional Arrays
+string[,] grid1 = new[,] {
+  { "A0", "B0", "C0", "D0" },
+  { "A1", "B1", "C1", "D1" },
+  { "A2", "B2", "C2", "D2" }
+};
+
+//WriteLine($"Lower bound of the first dimension is: {grid1.GetLowerBound(0)}");
+//WriteLine($"Upper bound of the first dimension is: {grid1.GetUpperBound(0)}");
+//WriteLine($"Lower bound of the second dimension is: {grid1.GetLowerBound(1)}");
+//WriteLine($"Upper bound of the second dimension is: {grid1.GetUpperBound(1)}");
+
+
+// loop through multi-dimensional array
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
 {
-    WriteLine(names2[i]);
+    for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+    }
 }
