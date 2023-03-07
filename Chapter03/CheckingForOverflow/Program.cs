09b47105aa2a9a0ce9b037ec33d84bf1c3867e6f
Nick <nsagias@gmail.com>
2023-03-07 16:44:59 -0500

    create checked and unchecked overflow

--- Chapter03/CheckingForOverflow/Program.cs
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"Increment +1: {x}");
+        x++;
+        WriteLine($"Increment +1: {x}");
+        x++;
+        WriteLine($"Increment +1: {x}");
+        x++;
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("Catching overflow exception");
+}
+
+
+
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+}
