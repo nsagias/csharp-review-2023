a3a209951405732d9b984f461d8fe5ea789ff3f9
Nick <nsagias@gmail.com>
2023-03-07 19:47:25 -0500

    use CalculateTax

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,5 @@
-﻿TimeTable(number: 8, size: 10);
+﻿// TimeTable(number: 8, size: 10);
+
+decimal purchaseAmount = 150;
+decimal taxToPay = CalculateTax(amount: purchaseAmount, twoLetterRegionCode: "GB");
+WriteLine($"Greate Britain tax on amount: {purchaseAmount} is {taxToPay} ");
