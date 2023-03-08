fce41c04dd17439cc12b203082278f2ef95fb260
Nick <nsagias@gmail.com>
2023-03-07 19:43:39 -0500

    add CalculateTax function

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -9,4 +9,46 @@
         }
         WriteLine();
     }
+
+    static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+    {
+        // declare variable
+        decimal rate = 0.0M;
+        switch (twoLetterRegionCode)
+        {
+            case "CH":
+                rate = 0.08M;
+                break;
+            case "DK":
+            case "NO":
+                rate = 0.25m;
+                break;
+            case "GB":
+            case "FR":
+                rate = 0.2M;
+                break;
+            case "HU":
+                rate = 0.27M;
+                break;
+            case "OR":
+            case "AK":
+            case "MT":
+                rate = 0.0M;
+                break;
+            case "ND":
+            case "WI":
+            case "ME":
+            case "VA":
+                rate = 0.5M;
+                break;
+            case "CA":
+                rate = 0.0825M;
+                break;
+            default:
+                rate = 0.06M;
+                break;
+
+        }
+        return amount * rate;
+    }
 }
