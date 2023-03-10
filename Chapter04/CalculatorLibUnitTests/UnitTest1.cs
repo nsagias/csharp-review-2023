751f25dc0e4be70753a1e8d2d40cd2ff44583c32
Nick <nsagias@gmail.com>
2023-03-09 23:21:58 -0500

    add content to tests

--- Chapter04/CalculatorLibUnitTests/UnitTest1.cs
+++ Chapter04/CalculatorLibUnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿namespace CalculatorLibUnitTests;
+﻿using CalculatorLib;
+namespace CalculatorLibUnitTests;
 
 public class CalculatorUnitTests
 {
@@ -6,8 +7,14 @@ public class CalculatorUnitTests
     public void TestAdding2and2()
     {
         // arrange
+        double a = 2;
+        double b = 2;
+        double expected = 4;
+        Calculator calc = new();
         // act
+        double actual = calc.Add(a, b);
         // assert
+        Assert.Equal(expected, actual);
 
     }
 
@@ -15,15 +22,27 @@ public class CalculatorUnitTests
     public void TestAdding2and3()
     {
         // arrange
+        double a = 2;
+        double b = 3;
+        double expected = 5;
+        Calculator calc = new();
         // act
+        double actual = calc.Add(a, b);
         // assert
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
-    public void TestAdding2and4Fails ()
+    public void TestAdding2and4NotEqual()
     {
         // arrange
+        double a = 2;
+        double b = 4;
+        double expected = 9;
+        Calculator calc = new();
         // act
+        double actual = calc.Add(a, b);
         // assert
+        Assert.NotEqual(expected, actual);
     }
 }
