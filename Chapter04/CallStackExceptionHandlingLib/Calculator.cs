29f57a1ae888b504b731a1d045757a14b56aba11
Nick <nsagias@gmail.com>
2023-03-10 00:00:34 -0500

    add example bad code

--- Chapter04/CallStackExceptionHandlingLib/Calculator.cs
+++ Chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -1,6 +1,18 @@
-﻿namespace CallStackExceptionHandlingLib;
-public class Class1
+﻿using static System.Console;
+namespace CallStackExceptionHandlingLib
 {
-
+    public class Calculator
+    {
+        public static void Gamma() // public so it can be called from outside
+        {
+            WriteLine("In Gamma");
+            Delta();
+        }
+        private static void Delta()
+        // private so it can only be called internally
+        {
+            WriteLine("In Delta");
+            File.OpenText("bad file path");
+        }
+    }
 }
-
